839edcaeb70ce1d6ec49395050bdc3ebc9089ab1
LucasSom <lsomacal@gmail.com>
2023-02-02 12:06:46 -0300

    Agregados símbolos válidos

--- procesar_imagenes.py
+++ procesar_imagenes.py
@@ -9,51 +9,49 @@ Created on Fri Dec 16 16:51:35 2022
 # procesar_imagenes.py
 # tiene que ser parecido a digitize,py
 
-#%% librerias
+# %% librerias
 import argparse
 import src.src
 
 # def procesar_img(path_in, path_out)
 desc = 'Software para transcripción de notas periodísticas'
 
-
 parser = argparse.ArgumentParser(description=desc, epilog="Fundación Sadosky - Procuración del Tesoro de la Nación")
 parser.add_argument('imgs',
-    help="Directorio donde se encuentran los archivos de imagen para digitalizar.",
-    type=str)
+                    help="Directorio donde se encuentran los archivos de imagen para digitalizar.",
+                    type=str)
 parser.add_argument('-r', '--reporte',
-    default='reporte_digitalizacion.xlsx',
-    help="Archivo de salida excel, con el reporte del proceso de digitalización.",
-    type=str)
+                    default='reporte_digitalizacion.xlsx',
+                    help="Archivo de salida excel, con el reporte del proceso de digitalización.",
+                    type=str)
 parser.add_argument('-s', '--salida',
-    default='resultados',
-    help="Directorio donde se guardan los resultados.",
-    type=str)
+                    default='resultados',
+                    help="Directorio donde se guardan los resultados.",
+                    type=str)
 parser.add_argument('--txt',
-    default='txt',
-    help="Nombre del directorio de salida de los textos digitalizados, dentro del directorio de resultados.",
-    type=str)
+                    default='txt',
+                    help="Nombre del directorio de salida de los textos digitalizados, dentro del directorio de resultados.",
+                    type=str)
 parser.add_arsgument('--modelo-corrector',
-    default='./resources/model_juridico.bin',
-    help="Archivo del modelo del corrector ortográfico JamSpell (archivo .bin).",
-    type=str)
+                     default='./resources/model_juridico.bin',
+                     help="Archivo del modelo del corrector ortográfico JamSpell (archivo .bin).",
+                     type=str)
+
 
 def main():
     args = parser.parse_args()
-    
-    
-    
-    
+
     ocr_files(path_in=args.imgs,
-        archivo_salida=args.reporte,
-        salida=args.salida,
-        tmp=args.tmp,
-        txts=args.txt,
-        tesseract=args.tesseract,
-        kodak=args.kodak,
-        umbral_palabrasdesconocidas=args.umbral_palabras_desconocidas,
-        umbral_diferenciaocr=args.umbral_diferencia_ocr,
-        reporte=args.reporte_detallado,
-        modelo_jamspell=args.modelo_corrector)
+              archivo_salida=args.reporte,
+              salida=args.salida,
+              tmp=args.tmp,
+              txts=args.txt,
+              tesseract=args.tesseract,
+              kodak=args.kodak,
+              umbral_palabrasdesconocidas=args.umbral_palabras_desconocidas,
+              umbral_diferenciaocr=args.umbral_diferencia_ocr,
+              reporte=args.reporte_detallado,
+              modelo_jamspell=args.modelo_corrector)
+
 
 main()
--- src/src.py
+++ src/src.py
@@ -5,7 +5,7 @@ Created on Fri Dec 16 16:58:38 2022
 
 @author: mcerdeiro
 """
-#%% librerias
+# %% librerias
 import cv2
 import numpy as np
 import pytesseract
@@ -14,64 +14,82 @@ import re
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-#%% directorios
+
+# %% directorios
 path_in = 'input_data/'
 path_out = 'out_data/'
-#%% funciones de preprocesamiento
+
+
+# %% funciones de preprocesamiento
 def preprocesamiento(img):
     # with open(img, 'r') as f:
     m = cv2.imread(img)
-    m = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)    
+    m = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)
     return m
-#%% funciones de transcripcion
+
+
+# %% funciones de transcripcion
 def ocr_tesseract(filename, path_in, path_out):
-    preprocessed_img = preprocesamiento(path_in+filename)
+    preprocessed_img = preprocesamiento(path_in + filename)
     if isinstance(preprocessed_img, np.ndarray):
         custom_config = r'-c tessedit_char_whitelist="AÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXYZaábcdeéfghiíjklmnñoópqrstuúvwxyz0123456789 -_/.,:;()"'
-        text = str(((pytesseract.image_to_string(preprocessed_img,lang="spa", config=custom_config))))
-        with open(str(path_out+filename), "wb") as f:
+        text = str((pytesseract.image_to_string(preprocessed_img, lang="spa", config=custom_config)))
+        with open(str(path_out + filename), "wb") as f:
             f.write(text.encode("utf-8"))
     return text
-#%% funciones de correccion del texto
-def inicializar_corrector(path_modelo = './herramientas/model_juridico.bin'):
+
+
+# %% funciones de correccion del texto
+def inicializar_corrector(path_modelo='./herramientas/model_juridico.bin'):
     corrector = jamspell.TSpellCorrector()
-    assert(corrector.LoadLangModel(path_modelo))
+    assert (corrector.LoadLangModel(path_modelo))
     return corrector
+
 
 def spellcheck(corrector, texto):
     return corrector.FixFragment(texto)
 
+
 def unir_saltos_linea(text):
-    text = text.replace('-\n', '')    
+    text = text.replace('-\n', '')
     return text.replace('_\n', '')
 
+
 def filtrar_simbolosvalidos(texto):
-    temp = re.sub("[^A-Za-z0-9áéíóúüñÁÉÍÓÚÜÑ.,;\s]", ' ', texto)
+    """
+    Reemplaza los símbolos no presentes en el diccionario español por espacios en blanco
+    """
+    temp = re.sub("[^A-Za-z0-9áéíóúüñÁÉÍÓÚÜÑ.,;:{}()\+\'\"!¡¿?°\[\]\-\s]", ' ', texto)
     # temp = re.sub("\n", ' ', temp)
     return re.sub(' +', ' ', temp)
+
 
 def es_basura(string):
     return all(len(x) < 3 for x in string.split(' '))
 
+
 def postprocesamiento(text, corrector):
     text = os.linesep.join([s for s in text.splitlines() if s])
     text = os.linesep.join([s for s in text.splitlines() if not es_basura(s)])
-    text = unir_saltos_linea(text)    
-    text = spellcheck(corrector, text)    
+    text = unir_saltos_linea(text)
+    text = spellcheck(corrector, text)
     # text = unir_saltos_linea(text) # por qué otra vez?
     text = filtrar_simbolosvalidos(text)
     # text = text.lower() # por qué?
-    text = spellcheck(corrector, text) # por qué otra vez?
+    text = spellcheck(corrector, text)  # por qué otra vez?
     # text = text.lower()
-    return text    
-#%% funciones de chequeo de las transcripciones   
+    return text
+
+
+# %% funciones de chequeo de las transcripciones
 def palabras_desconocidas(texto, corrector):
     cantidad = 0
     for palabra in texto.split():
         cantidad += 0 if corrector.WordIsKnown(palabra) else 1
-    return cantidad 
+    return cantidad
 
-def chequeo_calidad(data, output_file, umbral_palabrasdesconocidas = 0.015):
+
+def chequeo_calidad(data, output_file, umbral_palabrasdesconocidas=0.015):
     writer = pd.ExcelWriter(output_file, engine="xlsxwriter")
     workbook = writer.book
     percent_fmt = workbook.add_format({'num_format': '0.0%'})
@@ -79,11 +97,14 @@ def chequeo_calidad(data, output_file, umbral_palabrasdesconocidas = 0.015):
     data.to_excel(writer, index=False, sheet_name="reporte")
     worksheet = writer.sheets["reporte"]
     worksheet.set_column('B:C', 12, percent_fmt)
-    worksheet.conditional_format(f"B2:B{len(data.index) + 1}", {"type": "cell", "criteria": ">=", "value": umbral_palabrasdesconocidas, "format": emp_format})
+    worksheet.conditional_format(f"B2:B{len(data.index) + 1}",
+                                 {"type": "cell", "criteria": ">=", "value": umbral_palabrasdesconocidas,
+                                  "format": emp_format})
     writer.save()
 
-#%% 
-def procesar_imgs(path_in, path_out, modelo_corrector = 'herramientas/model_juridico.bin'):
+
+# %%
+def procesar_imgs(path_in, path_out, modelo_corrector='herramientas/model_juridico.bin'):
     corrector = inicializar_corrector(modelo_corrector)
     data = []
     for filename in os.listdir(path_in):
@@ -91,59 +112,31 @@ def procesar_imgs(path_in, path_out, modelo_corrector = 'herramientas/model_juri
             print(filename)
             texto = ocr_tesseract(filename, path_in, path_out)
             texto = postprocesamiento(texto, corrector)
-            pp_desc = palabras_desconocidas(texto,corrector)/len(texto.split())
-            data.append({'filename':filename, 'palabras_desconocidas':pp_desc})
-            with open(path_out+filename, "wb") as text_file:
+            pp_desc = palabras_desconocidas(texto, corrector) / len(texto.split())
+            data.append({'filename': filename, 'palabras_desconocidas': pp_desc})
+            with open(path_out + filename, "wb") as text_file:
                 text_file.write(texto.encode("utf-8"))
-#%%
+
+
+# %%
 # ejemplo
-img = path_in+'imagen1.tif'
+img = path_in + 'imagen1.tif'
 img_preprocesada = preprocesamiento(img)
-#%%
+# %%
 # cv2.imshow('ventana1', img_preprocesada)
 # cv2.waitKey(0)
-cv2.destroyAllWindows()    
-#%%
-plt.figure(figsize = (34,23))
-plt.imshow(img_preprocesada, cmap = 'gray')
-#%%
-img_preprocesada.shape
-#%%
+cv2.destroyAllWindows()
+# %%
+plt.figure(figsize=(34, 23))
+plt.imshow(img_preprocesada, cmap='gray')
+# %%
+print(img_preprocesada.shape)
+# %%
 texto_img = ocr_tesseract('imagen1.tif', path_in, path_out)
 corrector = inicializar_corrector()
 texto_img_corregido = postprocesamiento(texto_img, corrector)
 print(texto_img_corregido)
-#%%
-output_file = path_out+'resultados.xls'
+# %%
+output_file = path_out + 'resultados.xls'
 
-
-
-
-
-
-chequeo_calidad(texto_img_corregido, output_file=path_out+'resultados', umbral_palabrasdesconocidas = 0.015)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+chequeo_calidad(texto_img_corregido, output_file=path_out + 'resultados', umbral_palabrasdesconocidas=0.015)
