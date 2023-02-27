b271932402f26c0878c3f7b12953557835428158
LucasSom <lsomacal@gmail.com>
2023-02-27 11:28:18 -0300

    Permisos arreglados + eliminado ocr_files.py

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ sudo apt-get -y install swig4.0;
 
 mkdir input_data;
 mkdir out_data;
-sudo chmod 666 input_data;
-sudo chmod 666 out_data;
+sudo chmod 777 input_data;
+sudo chmod 777 out_data;
 
 cd src;
 git clone https://github.com/bakwc/JamSpell.git;
-sudo chmod 666 JamSpell;
+sudo chmod 777 JamSpell;
 
 cd JamSpell;
 sudo chmod 777 *;
